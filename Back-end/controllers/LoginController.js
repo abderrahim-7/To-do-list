f2d01d02b9c19d603395a0d3c35ae8623c1785b1
abderrahim-7 <abdrahimbenali36@gmail.com>
2026-01-19 19:13:30 +0100

    passwords are now crypted

--- Back-end/controllers/LoginController.js
+++ Back-end/controllers/LoginController.js
@@ -1,31 +1,40 @@
-const path = require("path")
-const client = require("../db")
+const path = require("path");
+const client = require("../db");
+const bcrypt = require("bcrypt");
 
-const getLoginPage = async (req,res) => {
-    res.sendFile(path.join(__dirname,"..","..","Front-end","Login.html"))
-}
+const getLoginPage = async (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "..", "Front-end", "Login.html"));
+};
 
-const Login = async (req,res) => {
-    try{
-        const {username,password} = req.body;
-        const Query = "select * from users where username = $1 and password = $2"
-        const Values = [username,password]
-        const result = await client.query(Query,Values);
-        if (result.rowCount == 0){
-            res.json({success : false});
+const Login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const query = "SELECT * FROM users WHERE username = $1";
+        const values = [username];
+
+        const result = await client.query(query, values);
+
+        if (result.rowCount === 0) {
+            return res.json({ success: false });
         }
-        else{
-            req.session.user = {username}
-            res.json({success : true});
+
+        const user = result.rows[0];
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.json({ success: false });
         }
-    }
-    catch(error){
+
+        req.session.user = { username };
+        res.json({ success: true });
+    } catch (error) {
         console.error("Database Error:", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
-}
+};
 
 module.exports = {
     getLoginPage,
     Login
-}
+};
